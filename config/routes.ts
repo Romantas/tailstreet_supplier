b169e3f25b9b4f56867a03fbc958d9cffd7a1fe1
BuildTools <romantas.trumpa@stud.vgtu.lt>
2021-04-11 23:43:39 +0300

    first commit

---
+++ config/routes.ts
@@ -0,0 +1,88 @@
+﻿export default [
+  {
+    path: '/',
+    component: '../layouts/BlankLayout',
+    routes: [
+      {
+        path: '/user',
+        component: '../layouts/UserLayout',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './User/login',
+          },
+        ],
+      },
+      {
+        path: '/',
+        component: '../layouts/SecurityLayout',
+        routes: [
+          {
+            path: '/',
+            component: '../layouts/BasicLayout',
+            authority: ['admin', 'user'],
+            routes: [
+              {
+                path: '/',
+                redirect: '/dashboard',
+              },
+              {
+                path: '/dashboard',
+                name: 'dashboard',
+                icon: 'smile',
+                component: './Dashboard',
+              },
+              {
+                path: '/admin',
+                name: 'admin',
+                icon: 'crown',
+                component: './Admin',
+                authority: ['admin'],
+                routes: [
+                  {
+                    path: '/admin/sub-page',
+                    name: 'sub-page',
+                    icon: 'smile',
+                    component: './Welcome',
+                    authority: ['admin'],
+                  },
+                ],
+              },
+              {
+                name: 'Services',
+                icon: 'profile',
+                path: '/services',
+                component: './Services',
+              },
+              {
+                name: 'Reservations',
+                icon: 'table',
+                path: '/reservations',
+                //component: './Reservations',
+              },
+              {
+                name: 'Employees',
+                icon: 'team',
+                path: '/employees',
+                //component: './Employees',
+              },
+              {
+                name: 'Profile',
+                icon: 'user',
+                path: '/profile',
+                //component: './Profile',
+              },
+            ],
+          },
+          {
+            component: './404',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    component: './404',
+  },
+];
